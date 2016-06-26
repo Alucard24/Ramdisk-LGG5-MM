4a6c58d1c1ef05c47dc568d979e188ea2bd24526
alucard24 <dmbaoh2@gmail.com>
2016-06-26 13:26:59 +0200

    Google Services battery drain fixer by Alcolawl@xda

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -262,6 +262,19 @@ fi;
 		echo "0" > /sys/fs/selinux/enforce;
 	fi;
 
+	# Google Services battery drain fixer by Alcolawl@xda
+	# http://forum.xda-developers.com/google-nexus-5/general/script-google-play-services-battery-t3059585/post59563859
+	pm enable com.google.android.gms/.update.SystemUpdateActivity
+	pm enable com.google.android.gms/.update.SystemUpdateService
+	pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+	pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
+	pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+	pm enable com.google.android.gsf/.update.SystemUpdateActivity
+	pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
+	pm enable com.google.android.gsf/.update.SystemUpdateService
+	pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
+	pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+
 	# apply lge triton setting
 	if [ "$lge_triton" == "on" ]; then
 		echo "1" > /sys/devices/system/cpu/triton/enable;
